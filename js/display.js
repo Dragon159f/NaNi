c4d26c955ab4da0dde5d5c06d543080cdaf45593
Jason <jasongiang159@gmail.com>
2018-11-07 17:21:06 -0800

    image append work

--- js/display.js
+++ js/display.js
@@ -1,9 +1,11 @@
-function display(){
-    $("#subbtn").click(function(){
-      var imgUrl = $("#imageInput").val();
-      var text = $("#textInput").val();
-      $("#thread").append(imgUrl);
-      $("#thread").append(text);
-    }
-)};
-  
+var array = {
+    Caption: "string",
+    imageURL: "https://media.giphy.com/media/12HZukMBlutpoQ/giphy.gif"
+}
+
+$("#subbtn").click(function(){
+  var imgUrl = $("#imageInput").val();   
+  var text = $("#textInput").val();
+  $("#content").append("<img src=" + imgUrl + ">");
+  $("#content").append(text);
+}); 
