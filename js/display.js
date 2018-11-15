ec69b671205b9dcd8395f4a47993ce809727eb01
Jason <jasongiang159@gmail.com>
2018-11-14 17:10:21 -0800

    added title display and linked js files

--- js/display.js
+++ js/display.js
@@ -1,11 +1,16 @@
-var array = {
+let array = {
     Caption: "string",
     imageURL: "https://media.giphy.com/media/12HZukMBlutpoQ/giphy.gif"
 }
 
 $("#subbtn").click(function(){
-  var imgUrl = $("#imageInput").val();   
-  var text = $("#textInput").val();
+  let titleText = $("#titleInput").val();
+  let imgUrl = $("#imageInput").val();   
+  let text = $("#textInput").val();
+  $("#content").append("<img src=" + titleText + ">");
   $("#content").append("<img src=" + imgUrl + ">");
   $("#content").append(text);
-}); 
+  alert(imgUrl);
+});   
+
+console.log("this works!");
