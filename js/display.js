2c9b7485727ed919e603dad95a34c806f270ee1f
Jason <jasongiang159@gmail.com>
2018-12-05 16:30:50 -0800

    made the firebase display onto the page

--- js/display.js
+++ js/display.js
@@ -2,6 +2,7 @@ $("#subbtn").click(function(){
   let titleText = $("#titleInput").val();
   let imgUrl = $("#imageInput").val();   
   let text = $("#textInput").val();
+  
   $("#content").append(titleText);
   $("#content").append("<img src=" + imgUrl + ">");
   $("#content").append(text);
@@ -9,7 +10,13 @@ $("#subbtn").click(function(){
 
 export function displayData(data){
   for(let name in data){
-   // $("#content").append();
+    //Appends the Title into the Content id
+    $("#content").append("<h1>" + data[name].Title + "</h1>");
+    console.log(data[name].Title);
+    //Appends the Caption into the Content id
+    $("#content").append("<h1>" + data[name].Caption + "</h1>");
+    console.log(data[name].Caption);
+    //Appends the Image into the Content id
     $("#content").append("<img src=" + data[name].ImageUrl + ">");
     console.log(data[name].ImageUrl);
     //$("#content").append();
